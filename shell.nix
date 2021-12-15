6bc4b97243a4ec4e34911e0d227af174417cc24e
Samyak S Sarnayak <samyak201@gmail.com>
2021-12-15 18:30:22 +0530

    meta: add RUST_SRC_PATH in shell.nix for rust-analyzer on nvim

--- shell.nix
+++ shell.nix
@@ -6,6 +6,7 @@ mkShell {
     rust-analyzer
     rustfmt
   ];
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   shellHook = ''
     alias run="cargo run"
     alias build="cargo build"
