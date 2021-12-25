bef25e1030b252adf82d0bb43f9f1648edb90267
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2021-12-25 18:51:19 +0530

    feat(errors): different kinds of errors
    Adding support for different kinds of errors via the enum SpressoErrors.
    So far there are three types of errors:
    - runtime
    - syntax
    - numeric

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,35 @@
 use std::fmt;
 
 #[derive(Debug, Clone)]
+pub enum SpressoError {
+    Runtime(RuntimeError),
+    Syntax(SyntaxError),
+    Numeric(NumericError),
+}
+
+impl fmt::Display for SpressoError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            SpressoError::Runtime(err) => write!(f, "{:?}", err),
+            SpressoError::Syntax(err) => write!(f, "{:?}", err),
+            SpressoError::Numeric(err) => write!(f, "{:?}", err),            
+        }
+    }
+}
+
+impl From<RuntimeError> for SpressoError {
+    fn from(err: RuntimeError) -> Self {
+        SpressoError::Runtime(err)
+    }
+}
+
+impl From<SyntaxError> for SpressoError {
+    fn from(err: SyntaxError) -> Self {
+        SpressoError::Syntax(err)
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct RuntimeError {
     pub err: String,
 }
@@ -20,5 +49,51 @@ impl From<String> for RuntimeError {
 impl fmt::Display for RuntimeError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Runtime Error: {}", self.err)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct SyntaxError {
+    pub err: String,
+}
+
+impl From<&str> for SyntaxError {
+    fn from(message: &str) -> Self {
+        SyntaxError { err: message.to_string() }
+    }
+}
+
+impl From<String> for SyntaxError {
+    fn from(message: String) -> Self {
+        SyntaxError { err: message }
+    }
+}
+
+impl fmt::Display for SyntaxError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Syntax Error: {}", self.err)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct NumericError {
+    pub err: String,
+}
+
+impl From<&str> for NumericError {
+    fn from(message: &str) -> Self {
+        NumericError { err: message.to_string() }
+    }
+}
+
+impl From<String> for NumericError {
+    fn from(message: String) -> Self {
+        NumericError { err: message }
+    }
+}
+
+impl fmt::Display for NumericError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Numeric Error: {}", self.err)
     }
 }
