9a21d8eada8d1f69504e16d180b4a175106e73ae
Samyak S Sarnayak <samyak201@gmail.com>
2025-12-27 16:16:12 +0530

    Reapply "perf: avoid one clone in parse"
    This reverts commit cbca8e2025f9010d68753fcb77d015283ba6ccc0.

--- src/lib.rs
+++ src/lib.rs
@@ -285,11 +285,11 @@ fn parse(tokens: &mut VecDeque<Token>) -> Result<Expr, SpressoError> {
         TokenType::CloseParen => {
             Err(SpressoError::from(SyntaxError::from("Unexpected ')'")).with_token(token))
         }
-        _ => Ok(Expr::from(ExprKind::Atom(parse_atom(token.clone())?)).with_token(token)),
+        _ => Ok(Expr::from(ExprKind::Atom(parse_atom(&token)?)).with_token(token)),
     }
 }
 
-fn parse_atom(token: Token) -> Result<Atom, SpressoError> {
+fn parse_atom(token: &Token) -> Result<Atom, SpressoError> {
     match token.type_ {
         TokenType::Number => {
             let text = token.text.clone();
@@ -302,17 +302,20 @@ fn parse_atom(token: Token) -> Result<Atom, SpressoError> {
                 return Ok(Atom::Number(Number::Float(num)));
             }
 
-            Err(SpressoError::from(SyntaxError::from("Could not parse number")).with_token(token))
+            Err(
+                SpressoError::from(SyntaxError::from("Could not parse number"))
+                    .with_token(token.clone()),
+            )
         }
         TokenType::Unit => Ok(Atom::Unit),
         TokenType::String => Ok(Atom::String(
             token.text[1..token.text.len() - 1].to_string(),
         )),
-        TokenType::Symbol => Ok(Atom::Symbol(token.text)),
+        TokenType::Symbol => Ok(Atom::Symbol(token.text.clone())),
         TokenType::OpenParen | TokenType::CloseParen | TokenType::Quote => Err(SpressoError::from(
             SyntaxError::from("Cannot extract atom from these lol"),
         )
-        .with_token(token)),
+        .with_token(token.clone())),
     }
 }
 
